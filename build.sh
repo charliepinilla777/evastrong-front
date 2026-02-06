b70fd5fb5d3f6bd52954a3af2fa879d8c5b7bd33
carlos andres pinilla <charliepinilla29@gmail.com>
2026-02-05 21:36:24 -0500

    Remove vercel.json configuration

--- build.sh
+++
@@ -1,9 +0,0 @@
-﻿#!/bin/bash
-curl
-https://storage.googleapis.com/flutter_infra_release/releases/
-stable/windows/flutter_windows_3.35.1-stable.zip -o
-flutter.zip
-unzip -q flutter.zip
-export PATH="$PATH:$(pwd)/flutter/bin"
-flutter pub get
-flutter build web --release
